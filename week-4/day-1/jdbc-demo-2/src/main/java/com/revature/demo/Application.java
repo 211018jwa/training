df1b525c43b0231988c670bcb4ee2ce9da5c1661
Bach Tran <bach.tran@revature.com>
2021-11-08 15:12:00 -0600

    Add jdbc-demo-2 with updated pom.xml for creating executable jar

---
+++ week-4/day-1/jdbc-demo-2/src/main/java/com/revature/demo/Application.java
@@ -0,0 +1,33 @@
+package com.revature.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.revature.controller.ExceptionMappingController;
+import com.revature.controller.StudentController;
+
+import io.javalin.Javalin;
+
+public class Application {
+
+	public static void main(String[] args) {
+				
+		Javalin app = Javalin.create();
+		
+		Logger logger = LoggerFactory.getLogger(Application.class);
+		
+		app.before(ctx -> {
+			logger.info(ctx.method() + " request received to the " + ctx.path() + " endpoint");
+		});
+		
+		StudentController controller = new StudentController();
+		controller.registerEndpoints(app);
+		
+		ExceptionMappingController exceptionController = new ExceptionMappingController();
+		exceptionController.mapExceptions(app);
+				
+		app.start();
+		
+	}
+
+}
