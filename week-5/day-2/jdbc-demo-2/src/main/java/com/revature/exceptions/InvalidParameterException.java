61a08937a2493edff926ad9026d600380a3f44c6
Bach Tran <bach.tran@revature.com>
2021-11-16 11:03:56 -0600

    Update frontend with error message display and revise backend to accomodate multiple invalid inputs

---
+++ week-5/day-2/jdbc-demo-2/src/main/java/com/revature/exceptions/InvalidParameterException.java
@@ -0,0 +1,31 @@
+package com.revature.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvalidParameterException extends Exception {
+	
+	public List<String> messages = new ArrayList<>();
+	
+	public InvalidParameterException() {
+		super();
+	}
+
+	public InvalidParameterException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public InvalidParameterException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidParameterException(String message) {
+		super(message);
+	}
+
+	public InvalidParameterException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ week-5/day-2/student-recordkeeping-frontend/index.js
@@ -0,0 +1,93 @@
+let url = 'ec2-18-219-69-82.us-east-2.compute.amazonaws.com';
+/* When the window object is loaded (load event), go ahead and fetch all of the data about students
+and populate the student table */
+window.addEventListener('load', getAndPopulateStudents);
+
+async function getAndPopulateStudents() {
+    let res = await fetch(`http://${url}:8080/students`);
+
+    let studentsArray = await res.json();
+
+    populateStudentTable(studentsArray);
+
+}
+
+function populateStudentTable(array) {
+    let tbody = document.querySelector('.main-section .table-container tbody');
+    tbody.innerHTML = '';
+    /*
+        we could iterate over the array using
+        1. traditional for loop ( for (let i = 0; i < ...; i++) {} )
+        2. for of: iterates over the actual elements (or values)
+        3. for in: iterates over the indices of whatever you are iterating over (or keys)
+
+    */
+
+    for (let personObject of array) {
+        let tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        td1.innerText = personObject.id;
+        let td2 = document.createElement('td');
+        td2.innerText = personObject.firstName;
+        let td3 = document.createElement('td');
+        td3.innerText = personObject.lastName;
+        let td4 = document.createElement('td');
+        td4.innerText = personObject.classification;
+        let td5 = document.createElement('td');
+        td5.innerText = personObject.age;
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+
+        tbody.appendChild(tr);
+    }
+
+}
+
+
+let studentSubmitButton = document.querySelector('#student-submit');
+studentSubmitButton.addEventListener('click', async () => {
+
+    let firstNameInputElement = document.querySelector('#first-name');
+    let lastNameInputElement = document.querySelector('#last-name');
+    let classificationDropdownElement = document.querySelector('#classification');
+    let ageInputElement = document.querySelector('#age');
+    
+    let personObjectToAdd = {
+        'firstName': firstNameInputElement.value,
+        'lastName': lastNameInputElement.value,
+        'classification': classificationDropdownElement.value,
+        'age': ageInputElement.value
+    };
+
+    let res = await fetch(`http://${url}:8080/students`, {
+    method: 'POST',
+    body: JSON.stringify(personObjectToAdd)
+    });
+
+    if (res.status === 400) {
+        let messageArray = await res.json(); // res.json() will return a promise, so we can await the promise
+        
+        let formErrorOutputElement = document.querySelector('#form-error-output');
+        formErrorOutputElement.innerHTML = '';
+
+
+        for (let i = 0; i < messageArray.length; i++) {
+            let message = messageArray[i];
+            
+            let pElement = document.createElement('p');
+            pElement.innerText = message;
+            formErrorOutputElement.appendChild(pElement);
+        }
+        
+    }
+    
+    /* JSON.stringify(obj) takes an object and converts it into a JSON String */
+    /* JSON.parse(string) takes a JSON string and turns it into a JavaScript object */
+
+    getAndPopulateStudents();
+});
